b88644e624093f4a51c10deef5d8fceed53dfac9
Do Hai Dang <dohaidang67890@gmail.com>
2026-04-03 20:46:34 +0700

    sửa lỗi người bán có thể end

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -4,11 +4,15 @@ public class Item {
     private String name;
     private String description;
     private int startPrice;
+    private String sellerID;
+    private boolean isEnded;
 
-    public Item(String name, String description, int startPrice) {
+    public Item(String name, String description, int startPrice, String sellerID) {
         this.name = name;
         this.description = description;
         this.startPrice = startPrice;
+        this.sellerID=sellerID;
+        this.isEnded=false;
     }
 
     public String getName() {
@@ -17,5 +21,13 @@ public class Item {
 
     public int getStartPrice() {
         return startPrice;
+    }
+
+    public String getSellerID() {
+        return sellerID;
+    }
+
+    public void endAuction(){
+        this.isEnded=true;
     }
 }
